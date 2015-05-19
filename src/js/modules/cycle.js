c71037c6b669d8e6e428676f3a3447bea01f63a9
Leo Ørsnes <zeekooster@gmail.com>
2015-05-19 17:34:37 +0200

    Working heatmap

--- src/js/modules/cycle.js
+++ src/js/modules/cycle.js
@@ -16,32 +16,38 @@ this.cycle = function() {
 		if (UXHand.HeatMap) {
 			var data = this._last;
 
-			if (data.start) {
+			if (data.start && data.start.pageX != 0 && data.start.pageY != 0) {
 				this.tracking.push({
 					x: data.start.pageX,
 					y: data.start.pageY,
+					pageX: data.start.pageX,
+					pageY: (data.start.pageY-window.pageYOffset),
 					count: 5
 				});
 			}
 
-			if (data.end) {
+			if (data.end && data.end[0].clientY != 0 && data.end[0].clientX != 0) {
 				this.tracking.push({
 					x: data.end[0].clientX,
 					y: data.end[0].clientY,
+					pageX: data.end[0].pageX,
+					pageY: (data.end[0].pageY-window.pageYOffset),
 					count: 5
 				});
 			}
 
-			[].forEach.call(data.drag, function(drag, index) {
-				if (drag.x == 0 && drag.y == 0) {
-					return;
-				}
-				UXHand.tracking.push({
-					x: drag.x,
-					y: drag.y,
-					count: 5 
+			if (data.drag.length != 0) {
+				[].forEach.call(data.drag, function(drag, index) {
+					console.log(drag.x, drag.y);
+					UXHand.tracking.push({
+						x: drag.x,
+						y: drag.y,
+						pageX: drag.pageX,
+						pageY: (drag.pageY-window.pageYOffset),
+						count: 5 
+					});
 				});
-			});
+			}
 
 		}
 
